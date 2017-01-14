3b52c71cf3914c4fedcf4d05a81ad9cd269f488b
Joubin Jabbari <joubin.j@gmail.com>
2017-01-13 17:56:59 -0800

    single page

--- Crawl.py
+++ Crawl.py
@@ -2,48 +2,32 @@ from ssl import CertificateError
 from urllib import parse, request, error
 
 import tldextract
-from bs4 import BeautifulSoup, SoupStrainer
 
-from Utils import URL as URL_UTIL
+from Page import Page
 
 
 class Crawl:
     def __init__(self, base_domain: str = None):
         self.__base_domain = base_domain
         self.__urls = dict()
-        self.__opener = request.build_opener()
-        self.__opener.addheaders = [('User-Agent', 'https://jabbari.io/contact.php')]
 
     def get_base_domain(self):
         return self.__base_domain
 
     def crawl(self, url: str = None):
+        print("Going in")
         if url is None:
             url = self.__base_domain
         if url is "#" or self.is_x_in_y(url) or url.endswith(".pdf"):
             return
-        try:
-
-            html = self.__opener.open(url).read()
-            soup = BeautifulSoup(html, "lxml", parse_only=SoupStrainer('a', href=True))
-            for link in soup.find_all('a'):
-                tmp_link = link['href']
-                if not URL_UTIL.validate_url(tmp_link) and tmp_link != "#":
-                    tmp_link = URL_UTIL.make_url_from_base(url, tmp_link)
-                if URL_UTIL.validate_url(tmp_link) and self.is_same_domain(tmp_link):
-                    self.add_new_url(tmp_link)
-                    self.set_done_scanning(url)
-                    self.crawl(tmp_link)
-        except error.HTTPError:
-            pass
-        except TimeoutError:
-            pass
-        except CertificateError:
-            pass
-        except error.URLError:
-            pass
-        except Exception:
-            pass
+        print("Url", url)
+        page = Page(url)
+        self.set_done_scanning(url)
+        print(len(page.get_links()))
+        for link in page.get_links():
+            print("-->", link)
+            self.add_new_url(link)
+            self.crawl(link)
 
     def is_same_domain(self, url: str, include_sub: bool = True) -> bool:
         if include_sub:
@@ -76,10 +60,11 @@ if __name__ == '__main__':
 
     start = time.time()
     sys.setrecursionlimit(sys.getrecursionlimit() * 2)
-    crawl = Crawl("http://uber.com")
+    crawl = Crawl("http://jabbari.io")
     crawl.crawl()
     end = time.time()
     print(crawl.get_url_count())
     print("Took ", (end - start), " seconds")
-
+    # page = Page("https://jabbari.io/blog.php")
+    # print(page.get_links())
     # crawl.print_urls()
--- Page.py
+++ Page.py
@@ -1,3 +1,6 @@
+from plistlib import Dict
+from typing import List
+
 from bs4 import BeautifulSoup, SoupStrainer
 from Utils import URL as URL_UTIL, IO as IO_UTIL
 
@@ -5,7 +8,8 @@ from Utils import URL as URL_UTIL, IO as IO_UTIL
 class Page:
     def __init__(self, url: str = None):
         self.url = url
-        self.__soup = BeautifulSoup(URL_UTIL.get_content_of_url(url), "lxml", parse_only=SoupStrainer('a', href=True))
+        response = URL_UTIL.get_content_of_url(url)
+        self.__soup = BeautifulSoup(response, "lxml", parse_only=SoupStrainer('a', href=True))
         self.links = list()
         self.__get_links()
 
@@ -13,12 +17,18 @@ class Page:
         self.links = list()
         for link in self.__soup.find_all('a'):
             tmp_link = link['href']
+            print("=======>", tmp_link)
             if not URL_UTIL.validate_url(tmp_link) and tmp_link != "#":
                 tmp_link = URL_UTIL.make_url_from_base(self.url, tmp_link)
+                print("=======>", tmp_link)
+
             if URL_UTIL.validate_url(tmp_link) and URL_UTIL.is_same_domain(self.url, tmp_link):
                 self.links.append(tmp_link)
+                print("=======>", tmp_link)
+
+    def get_links(self) -> List:
+        return self.links
 
     def __write_to_file(self):
         with open(IO_UTIL.get_html_cache_with_file(self.url), mode='w+') as file:
             file.write(self.__soup)
-
